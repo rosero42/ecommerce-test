3f7f96304a2a8aa8cbfcb9a236dee4c0db6d8563
Rosemary Rosvanis <rosemaryrosvanis@tecace.com>
2023-08-07 14:09:07 -0700

    Allow to create users in the firebase DB

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import { signInWithGooglePopup } from "../../../util/firebase/firebase.utils" 
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../../util/firebase/firebase.utils" 
 
 const SignIn = () =>{
     const logGoogleUser = async () =>{
-        const response = await signInWithGooglePopup()
-        console.log("@Rosvan "+response)
+        const {user} = await signInWithGooglePopup()
+        const userDocRef = await createUserDocumentFromAuth(user)
     }
     return(
         <div>
--- src/util/firebase/firebase.utils.js
+++ src/util/firebase/firebase.utils.js
@@ -1,11 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { 
     getAuth, 
-    signIn, 
     signInWithRedirect, 
     signInWithPopup, 
     GoogleAuthProvider 
 } from 'firebase/auth'
+import { 
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -28,3 +33,30 @@ const firebaseConfig = {
   export const auth = getAuth()
 
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+
+  export const db = getFirestore()
+
+  export const createUserDocumentFromAuth = async (userAuth) =>{
+    const userDocRef = doc(db, 'users', userAuth.uid)
+    console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef);
+    
+    if(!userSnapshot.exists()){
+      const {displayName, email} = userAuth
+      const createdAt = new Date()
+      try{
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        })
+      }catch(ex){
+        console.log('exception creating the user', ex.message)
+      }
+    }
+
+    return userDocRef
+  }
+
