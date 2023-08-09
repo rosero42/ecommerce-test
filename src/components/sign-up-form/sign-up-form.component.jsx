7a302152a242dd3ebbdd3a72f9a91e32d9ffe99f
Rosemary Rosvanis <rosemaryrosvanis@tecace.com>
2023-08-08 17:53:33 -0700

    Add create user with email and password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../util/firebase/firebase.utils"
 
 const defaultFormFields = {
     displayName: '',
@@ -11,6 +12,28 @@ const SignUpForm = () =>{
     const [formField, setFormFields] = useState(defaultFormFields)
     const { displayName, email, password, confirmPassword } = formField
 
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+      };
+
+    const handleSubmit = async (event) =>{
+        console.log("Handle submit")
+        event.preventDefault()
+        if(password !== confirmPassword){
+            alert("Passwords do not match!!!")
+            return
+        }
+        
+        try{
+            const {user} = await createAuthUserWithEmailAndPassword(email,password)
+            await createUserDocumentFromAuth(user, {displayName})
+            resetFormFields()
+
+        }catch(ex){
+            console.log(ex)
+        }
+    }
+
     const handleChange = (event) =>{
         const {name, value} = event.target
         setFormFields({...formField, [name]: value})
@@ -18,7 +41,7 @@ const SignUpForm = () =>{
     return(
         <div>
             <h1>Sing up with your email and password</h1>
-            <form onSubmit={() => {}}>
+            <form onSubmit={handleSubmit}>
                 <label>Display Name</label>
                 <input 
                     type="text" 
--- src/util/firebase/firebase.utils.js
+++ src/util/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import {
     getAuth, 
     signInWithRedirect, 
     signInWithPopup, 
-    GoogleAuthProvider 
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth'
 import { 
     getFirestore,
@@ -38,9 +39,10 @@ const firebaseConfig = {
 
   export const db = getFirestore()
 
-  export const createUserDocumentFromAuth = async (userAuth) =>{
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) =>{
+    console.log('create user doc from auth')
+    if(!userAuth) return
     const userDocRef = doc(db, 'users', userAuth.uid)
-    console.log(userDocRef)
 
     const userSnapshot = await getDoc(userDocRef);
     
@@ -51,7 +53,8 @@ const firebaseConfig = {
         await setDoc(userDocRef, {
           displayName,
           email,
-          createdAt
+          createdAt,
+          ...additionalInformation,
         })
       }catch(ex){
         console.log('exception creating the user', ex.message)
@@ -61,3 +64,8 @@ const firebaseConfig = {
     return userDocRef
   }
 
+
+export const createAuthUserWithEmailAndPassword = async (email, password) =>{
+  if(!email || !password) return
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
