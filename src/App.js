d431c283cb5f32e0c946729734390edd4d5f919d
Rosemary Rosvanis <rosemaryrosvanis@tecace.com>
2023-08-10 17:26:20 -0700

    Add authentication to styling page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route} from 'react-router-dom'
 import Home from "./components/routes/home/home.component";
 import Navigation from './components/routes/navigation/navigation.component';
-import Authentication from './components/routes/authentication/authentication';
+import Authentication from './components/routes/authentication/authentication.component';
 const App = () => {
 
 
--- src/components/routes/authentication/authentication.component.jsx
+++ src/components/routes/authentication/authentication.component.jsx
@@ -1,22 +1,13 @@
-import { useEffect } from "react"
-import { 
-    auth,
-    signInWithGooglePopup, 
-    createUserDocumentFromAuth,
-    signInWithGoogleRedirect 
-} from "../../../util/firebase/firebase.utils" 
-import { getRedirectResult } from "firebase/auth"
-
 import SignUpForm from "../../sign-up-form/sign-up-form.component"
 import SignInForm from "../../sign-in-form/sign-in-form.component"
+import './authentication.styles.scss'
 
 const Authentication = () =>{
 
 
     
     return(
-        <div>
-            <h1>Sign In Page</h1>
+        <div className="authentication-container">
             <SignInForm></SignInForm>
             <SignUpForm></SignUpForm>
 
