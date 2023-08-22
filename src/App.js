312604fbb730b3045f949bc9fe2edf098809ee06
Rosemary Rosvanis <rosemaryrosvanis@tecace.com>
2023-08-22 16:37:09 -0700

    Set up basic shop page and styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import { Routes, Route} from 'react-router-dom'
 import Home from "./components/routes/home/home.component";
 import Navigation from './components/routes/navigation/navigation.component';
 import Authentication from './components/routes/authentication/authentication.component';
+import Shop from './components/routes/shop/shop.component';
 const App = () => {
 
-
-
-  const Shop =()=>{
-    return <h1>I am the shop page</h1>
-  }
 
   return (
     <Routes>
