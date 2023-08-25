3c2c24849bae78518f9f31530cc9906646dc2b60
Rosemary Rosvanis <rosemaryrosvanis@tecace.com>
2023-08-25 14:44:45 -0700

    Add ability for cart icon to track number of items in count

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
 
@@ -17,17 +17,24 @@ export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen:() =>{},
     cartItems: [],
-    addItemToCart: () =>{}
+    addItemToCart: () =>{},
+    numItems: 0,
 });
 
 export const CartProvider = ({children}) =>{
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [numItems, setNumItems] = useState(0);
+
+    useEffect(() =>{
+        const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity,0)
+        setNumItems(newCartCount)
+    }, [cartItems])
 
     const addItemToCart = (productToAdd) =>{
         setCartItems(addCartItem(cartItems, productToAdd))
     }
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, numItems};
     return(
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     );
