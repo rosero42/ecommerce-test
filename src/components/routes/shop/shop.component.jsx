953d1c0cf653818752f00c4d9cb0d4563a28402f
Rosemary Rosvanis <rosemaryrosvanis@tecace.com>
2023-08-25 20:40:07 -0700

    Add preview for categories on the shopping page

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,26 +1,18 @@
-import { useContext, Fragment } from "react"
-import ProductCard from "../../product-card.component/product-card.component"
+import { useContext } from "react"
 import './shop.styles.scss'
 import { CategoriesContext } from "../../../contexts/categories.context"
+import CategoryPreview from "../../category-preview/category-preview.component"
 const Shop = () =>{
     const{categoriesMap} = useContext(CategoriesContext)
 
     return(
-        <Fragment>
-            {Object.keys(categoriesMap).map((title) => (
-                    <Fragment key={title}>
-                        <h2>{title}</h2>
-                        <div className='products-container'>
-                            {categoriesMap[title].map((product) => (
-                                <ProductCard key={product.id} product={product} />
-                            ))}
-                        </div>
-                    </Fragment>
-
-            ))}
-        </Fragment>
+        <div className="shop-container">
+            {Object.keys(categoriesMap).map((title) =>{
+                const products = categoriesMap[title]
+                return <CategoryPreview key={title} title={title} products={products}/>
+            })}
+        </div>
     )
 }
 
 export default Shop
-
