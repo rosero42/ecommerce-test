b0eab80061cf117381201203a6b57bfeac266008
Rosemary Rosvanis <rosemaryrosvanis@tecace.com>
2023-08-28 16:35:44 -0700

    Make button componenet to use styled components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,6 +1,6 @@
 import FormInput from "../form-input/form-input.component"
 import {  useState } from "react"
-import Button from "../button/button.component"
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component"
 import './sign-in-form.styles.scss'
 import { 
     signInWithGooglePopup, 
@@ -55,7 +55,7 @@ const SignInForm = () =>{
                 <FormInput label='Password' type='password'required name='password' onChange={handleChange} value={password} ></FormInput> 
                 <div className="buttons-container">
                     <Button type='submit'>Sign In</Button>
-                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+                    <Button type='button' buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google Sign In</Button>
                 </div>
 
             </form>
