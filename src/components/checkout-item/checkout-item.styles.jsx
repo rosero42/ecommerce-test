3280a7f37ee659c0a9f9a3133d5b8a3d618beb65
Rosemary Rosvanis <rosemaryrosvanis@tecace.com>
2023-08-29 14:03:11 -0700

    Bug fix: Image doesn't show on checkout items

--- src/components/checkout-item/checkout-item.styles.jsx
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -14,27 +14,20 @@ export const CheckoutItemContainer = styled.div`
 export const ImageContainer = styled.div`
   width: 23%;
   padding-right: 15px;
+
+  img{
+    width: 100%;
+    height: 100%;
+  }
 `
-export const ImageStyle = styled.image`
-  width: 100%;
-  height: 100%;
+export const BaseSpan = styled.span`
+  width:23%;
 `
 
-export const Name = styled.span`
-  width: 23%;
-
-`
-export const Quantity = styled.span`
-  width: 23%;
+export const Quantity = styled(BaseSpan)`
   display: flex;
-
-
 `
-export const Price = styled.span`
-  width: 23%;
-
-`
-export const Value = styled.div`
+export const Value = styled.span`
   margin: 0 10px;
 `
 export const Arrow = styled.div`
