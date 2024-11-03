bb8b45d39bd2b4da661a74a7fd07561cc95d9595
Rosemary <rosvanis.r@gmail.com>
2024-11-02 17:22:16 -0700

    Make category fetching async

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -4,18 +4,13 @@ import { useDispatch } from 'react-redux'
 import './shop.styles.scss'
 import CategoriesPreview from "../categories-preview/categories-preview.component"
 import Category from "../category/category.component"
-import { setCategories } from "../../../store/categories/category.action"
-import { getCategoriesAndDocuments } from '../../../util/firebase/firebase.utils'
+import { fetchCategoriesAsync } from "../../../store/categories/category.action"
 
 const Shop = () =>{
     const dispatch = useDispatch()
 
   useEffect(()=>{
-    const getCategoriesMap = async () =>{
-      const categoriesArray = await getCategoriesAndDocuments('categories')
-      dispatch(setCategories(categoriesArray))
-    }
-    getCategoriesMap()
+    dispatch(fetchCategoriesAsync())
   },[dispatch])
 
     return(
